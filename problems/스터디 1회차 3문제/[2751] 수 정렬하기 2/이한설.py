e214b7cb2e4231482c738e7f402d0507550d8b92
Hanse01 <97111689+Hanse01@users.noreply.github.com>
2025-05-19 11:44:46 +0900

    Create 이한설.py
    counting star

---
+++ problems/스터디 1회차 3문제/[2751] 수 정렬하기 2/이한설.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+OFFSET = 1000000  # 음수 포함을 위해 offset 사용
+SIZE = 2000001    # -1,000,000 ~ 1,000,000 범위
+
+# 카운팅 배열 생성
+count = [False] * SIZE
+
+for _ in range(N):
+    num = int(input())
+    count[num + OFFSET] = True  # 존재 표시
+
+# 출력
+for idx in range(SIZE):
+    if count[idx]:
+        print(idx - OFFSET)
